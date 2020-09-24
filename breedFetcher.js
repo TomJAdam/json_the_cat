2b79b0b336dea451e57d1e1e5f3ee42bbac14dfa
Tom Adam <tom.adam21@gmail.com>
2020-09-24 17:52:42 +0000

    implemented breedFetcher.js logic

---
+++ breedFetcher.js
@@ -0,0 +1,20 @@
+//Json the cat
+// command line app for fetching cat data using theCatApi
+
+const request = require('request');
+//node line input
+const input = process.argv.splice(2);
+
+//searches theCatApi for a breed and returns the description
+request(`https://api.thecatapi.com/v1/breeds/search?q=${input[0]}`, (error, response, body) => {
+  if (error) {
+    console.log('error:', error);
+  } else {
+    const data = JSON.parse(body);
+    console.log('description:', data[0].description);
+    if (Object.keys(data).length === 0) {
+      console.log('Please enter valid cat breed.');
+    }
+  }
+});
+
