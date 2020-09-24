f79b9636fcc90ad33b455c2148f158c1857b1625
Tom Adam <tom.adam21@gmail.com>
2020-09-24 19:16:49 +0000

    refactored function and added index.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,21 +2,23 @@
 // command line app for fetching cat data using theCatApi
 
 const request = require('request');
-//node line input
-const input = process.argv.splice(2);
+
 
 //searches theCatApi for a breed and returns the description
-request(`https://api.thecatapi.com/v1/breeds/search?q=${input[0]}`, (error, response, body) => {
-  if (error) {
-    console.log('error:', error);
-  } else {
-    const data = JSON.parse(body);
-    if (data[0] !== undefined) {
-      console.log('description:', data[0].description);
-    }
-    if (Object.keys(data).length === 0) {
-      console.log('Please enter valid cat breed.');
-    }
-  }
-});
 
+
+const fetchBreedDescription = (breedName, callback) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      return callback(error, null);
+    }
+    const data = JSON.parse(body);
+    if (data[0]) {
+      callback(null, data[0].description);
+    } else {
+      callback('Please enter valid cat breed.');
+    }
+  });
+};
+
+module.exports = fetchBreedDescription;
